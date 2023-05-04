f4dabde35ed3e7a387b894341e94fe66e9b496a4
Carolina Mamani <111472448+CMCNY@users.noreply.github.com>
2023-05-04 18:38:18 -0300

    Delete NumerosIgualesException.py

--- Python/Excepciones/Leccion01/NumerosIgualesException.py
+++
@@ -1,7 +0,0 @@
-#Creamos nuestra propia excepcion usando la clase Padre
-
-class NumerosIgualesException(Exception): #Extiende de la clase
-    def __init__(self, mensaje):
-        self.message= mensaje # hemos usado el atributo de la clase padre Exception
-
-        
