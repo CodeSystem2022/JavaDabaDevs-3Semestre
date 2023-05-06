62a62171603c8ff836f0c1a5ff56e8f7407e3895
David <110946400+Davo-e@users.noreply.github.com>
2023-05-06 10:17:28 -0300

    Add files via upload
    Realizando ejercicio, Ciclos.

---
+++ ejercicioCiclos.js
@@ -0,0 +1,15 @@
+//Ejercitando Ciclos, While.
+let conteo = 0;
+while (conteo <= 7){
+    console.log(conteo);
+    conteo++;
+};
+console.log("Fin del ciclo While");
+//Ejercitando Ciclos, For
+
+for(let contadore = 0; contadore <=10; contadore++){
+    if(contadore % 2 == 0){
+    }
+    console.log(contadore);
+};
+console.log("Fin del Ciclo For");
