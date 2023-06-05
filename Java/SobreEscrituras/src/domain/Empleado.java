03aab1da972d5bb410a761d6387825680b3fc2f2
David <110946400+Davo-e@users.noreply.github.com>
2023-06-05 00:12:02 -0300

    subiendo clase 4, sobreescritura.

---
+++ Java/SobreEscrituras/src/domain/Empleado.java
@@ -0,0 +1,38 @@
+/*
+
+ */
+package domain;
+
+
+public class Empleado {
+    protected String nombre;
+    protected double sueldo;
+    
+    //COnstructor 
+    public Empleado(String nombre, double sueldo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+    
+    //Método para la sobre escritura
+    public String obtenerDetalles(){
+        return "Nombre: "+this.nombre+", Sueldo: "+this.sueldo;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+}
---
+++ Java/SobreEscrituras/src/test/TesteSobreEscritura.java
@@ -0,0 +1,23 @@
+/*
+El polimorfismo te permite tener un método más generico 
+ */
+package test;
+
+import domain.*;
+
+
+public class TesteSobreEscritura {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juan", 10000);
+        imprimir(empleado1);
+//System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+        empleado1 = new Gerente("Jose", 5000, "Sistemas");
+        imprimir(empleado1);
+//System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+    }
+    
+    public static void imprimir(Empleado empleado){
+        String detalles = empleado.obtenerDetalles();
+        System.out.println("detalles = " + detalles);
+    }
+}
