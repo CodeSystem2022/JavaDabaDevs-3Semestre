fb5d20c71635a71e107ba581764c51b142dd3ef8
Carolina Mamani <mamanicarolina27@gmail.com>
2023-06-05 00:05:01 -0300

    Subiendo clases abastractas java s6

---
+++ Java/Leccion05/ClasesAbstractas/src/test/testAbstractas.java
@@ -0,0 +1,11 @@
+package test;
+
+import domain.*;
+
+
+public class testAbstractas {
+    public static void main(String[] args) {
+        FiguraGeometrica figura = new Rectangulo("Rectangulo");
+        figura.dibujar();
+    }
+}
